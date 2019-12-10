be59a1680f9f773bfc0b66f58c80bef88bec26c9
pascal-zarrad <P.Zarrad@outlook.de>
2019-12-11 00:08:51 +0100

    Update dependencies

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,14 @@
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
 
+import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
 @Component({
     components: {
         HelloWorld
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,7 @@
             For a guide and recipes on how to configure / customize this
             project,<br />
             check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-                >vue-cli documentation</a
-            >.
+            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
         </p>
         <h3>Installed CLI Plugins</h3>
         <ul>
